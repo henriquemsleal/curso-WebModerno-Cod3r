d372346c0844c49ff92117f326f3aea00054c538
henriquemsleal <henriquemsleal@gmail.com>
2021-08-18 20:13:06 -0300

    Operadores aritméticos

---
+++ Exercicios-JS/fundamentos/aritmeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+// operadores binários
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+//operador unário
+console.log(-divisao)
