d853dbc70027073bcd9c789694e42d1c423388a4
henriquemsleal <henriquemsleal@gmail.com>
2022-08-06 18:51:11 -0300

    Parâmetros entre módulos

---
+++ Exercicios-JS/node/passandoParametrosCliente.js
@@ -0,0 +1,5 @@
+/* const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'João');
+console.log(saudacoes);
+ */ // ou
+const saudacoes = require('./passandoParametros');
+console.log(saudacoes('Ana', 'Lucas', 'João'));
